3299f0c7411df21017b4a43a9e1dae0b0063f401
Bishal Adhikari <54226340+BishalAdhikari64@users.noreply.github.com>
2021-08-29 09:06:12 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Database Administrator", "Developer", "Designer", "Freelancer"],
+        strings: ["Software Engineer", "Database Administrator", "Database Designer", "Database Developer",  "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Database Administrator", "Developer", "Designer", "Freelancer"],
+        strings: ["Software Engineer", "Database Administrator", "Database Designer", "Database Developer",  "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
